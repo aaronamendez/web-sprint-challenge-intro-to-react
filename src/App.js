ffacd57b53ae26f4d52d1d2b491521a4235b4f82
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-12-10 14:51:46 -0500

    Created the axios request, and started on character component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import './App.css';
+// Dependencies
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// Components
+import Character from "./components/Character";
+import "./App.css";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
+	const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
+	useEffect(() => {
+		const fetchData = () => {
+			axios
+				.get("https://swapi.dev/api/people")
+				.then((res) => {
+					console.log(res.data);
+					setCharacters(res.data);
+				})
+				.catch((err) => {
+					console.err(err);
+				});
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1 className="Header">Characters</h1>
+			{characters.forEach((char) => {
+				return <Character data={char} />;
+			})}
+		</div>
+	);
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+// Import
+import React from "react";
+
+// Create Component
+const Character = (props) => {
+	const { data } = props;
+	return (
+		<div>
+			<h1>{data.name}</h1>
+		</div>
+	);
+};
+
+// Export
+export default Character;
